dde8b5e1248e1a21edb0fb6a83fff5d1b8f3d81e
s-fletcher <sfletchertx@gmail.com>
2020-05-08 14:07:12 -0500

    Initial setup

---
+++ README.md
@@ -0,0 +1,14 @@
+# Stats
+
+This project visualizes all Hacklahoma's past metrics to be viewable by the team.
+
+This project is made with [Keystone.js](https://www.keystonejs.com/) and [Next.js](https://nextjs.org/).
+
+## Running the Project.
+
+To run this project first run `npm install`.
+
+Before starting, setup a postgres database and update the
+environmental variable `DATABASE_URL` if needed.
+
+To start this project, run `npm run dev`.
---
+++ index.js
@@ -0,0 +1,36 @@
+const { Keystone } = require("@keystonejs/keystone");
+const { GraphQLApp } = require("@keystonejs/app-graphql");
+const { AdminUIApp } = require("@keystonejs/app-admin-ui");
+const { KnexAdapter: Adapter } = require("@keystonejs/adapter-knex");
+const { NextApp } = require("@keystonejs/app-next");
+const dotenv = require("dotenv");
+
+// Getting environmental variables
+dotenv.config();
+
+
+// Configuration for initiating keystone
+const PROJECT_NAME = "stats";
+const adapterConfig = {
+    dropDatabase: true,
+    knexOptions: { connection: process.env.DATABASE_URL },
+};
+
+
+// Initiating keystone
+const keystone = new Keystone({
+    name: PROJECT_NAME,
+    adapter: new Adapter(adapterConfig),
+});
+
+
+// Export
+module.exports = {
+    keystone,
+    apps: [
+        new GraphQLApp(),
+        new AdminUIApp({ enableDefaultRoute: false }),
+        new NextApp({ dir: "src" }),
+        "dist",
+    ],
+};
