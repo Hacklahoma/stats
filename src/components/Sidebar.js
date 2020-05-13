01d423af54b77a06a084bc56ea5ffd6cf377acf1
s-fletcher <sfletchertx@gmail.com>
2020-05-13 16:03:16 -0500

    Implemented sidebar

---
+++ src/components/Sidebar.js
@@ -0,0 +1,96 @@
+import styled from "styled-components";
+import Link from "next/link";
+import Router from "next/router";
+import { FiPieChart, FiEdit2, FiLogOut } from "react-icons/fi";
+import { MdAttachMoney } from "react-icons/md";
+import { Button, Tooltip } from "@material-ui/core";
+
+const StyledSidebar = styled.div`
+    background: #fcfcfc;
+    box-shadow: 0 0 4px rgba(0, 0, 0, 0.15);
+    height: 100vh;
+    width: 92px;
+    position: fixed;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    overflow: scroll;
+    .logo {
+        margin: 40px 0 8vh 0;
+        img {
+            width: 62px;
+        }
+    }
+    .button {
+        margin: 5px 0;
+        .icon {
+            width: 32px;
+            height: 32px;
+            color: #444;
+            opacity: 0.8;
+            transition: opacity 0.25s;
+        }
+    }
+    .button:hover {
+        .icon {
+            opacity: 1;
+        }
+    }
+    .disabled {
+        cursor: not-allowed !important;
+    }
+    .button:last-of-type {
+        margin-bottom: 20px;
+        margin-top: auto;
+    }
+`;
+function logout() {
+    Router.replace("/login", { shallow: true });
+}
+
+function Sidebar() {
+    return (
+        <StyledSidebar>
+            {/* Logo */}
+            <Link href="/">
+                <a className="logo">
+                    <img src="/images/logo192.png" alt="" />
+                </a>
+            </Link>
+            <div className="tabs">
+                {/* Metrics */}
+                <Tooltip title="Metrics" arrow placement="right">
+                    <Link href="/">
+                        <Button className="button">
+                            <FiPieChart className="icon" />
+                        </Button>
+                    </Link>
+                </Tooltip>
+                {/* Finances */}
+                <Tooltip title="Finances" arrow placement="right">
+                    <Button disableRipple className="button disabled">
+                        <MdAttachMoney className="icon" />
+                    </Button>
+                </Tooltip>
+                {/* Admin panel */}
+                <Tooltip title="Admin panel" arrow placement="right">
+                    <Link href="/admin">
+                        <Button className="button">
+                            <FiEdit2 className="icon" />
+                        </Button>
+                    </Link>
+                </Tooltip>
+            </div>
+            {/* Logout */}
+            <Tooltip title="Log out" arrow placement="right">
+                <Link href="/login">
+                    <Button onClick={() => logout()} className="button">
+                        <FiLogOut className="icon" />
+                    </Button>
+                </Link>
+            </Tooltip>
+        </StyledSidebar>
+    );
+}
+
+export default Sidebar;
