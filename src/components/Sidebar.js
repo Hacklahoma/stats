bebc06f69bf6582cfd5d6054d09c8b73c4c47829
s-fletcher <sfletchertx@gmail.com>
2020-05-17 21:36:23 -0500

    Implemented mobile sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,10 +1,12 @@
 import styled from "styled-components";
 import Link from "next/link";
-import Router from "next/router";
-import { FiPieChart, FiEdit2, FiLogOut } from "react-icons/fi";
+import { FiPieChart, FiBarChart2, FiX, FiEdit2, FiLogOut } from "react-icons/fi";
 import { MdAttachMoney } from "react-icons/md";
-import { Button, Tooltip } from "@material-ui/core";
+import { Button, Tooltip, ClickAwayListener } from "@material-ui/core";
+import { useState, useEffect } from "react";
+import { CSSTransition } from "react-transition-group";
 
+// Styling for desktop sidebar
 const StyledSidebar = styled.div`
     animation: slide-in 0.25s;
     background: #fcfcfc;
@@ -60,49 +62,214 @@ const StyledSidebar = styled.div`
     }
 `;
 
+// Styling for mobile menu
+const MENU_HEIGHT = "240px";
+const StyledHamburger = styled.div`
+    position: fixed;
+    .hamburgerButton {
+        margin: 20px;
+        padding: 8px 0;
+        .hamburger {
+            transform: rotate(90deg);
+            height: 38px;
+            width: 38px;
+            color: #1d1d1d;
+        }
+    }
+    .expanded {
+        cursor: pointer;
+        width: 100vw;
+        height: 100vh;
+        background: rgba(0, 0, 0, 0.15);
+        .container {
+            cursor: auto;
+            background: white;
+            height: ${MENU_HEIGHT};
+            overflow: hidden;
+            box-shadow: 0 3px 4px rgba(0, 0, 0, 0.15);
+            .item {
+                margin: 8px 20px;
+                .button {
+                    padding: 10px 20px;
+                    width: 100%;
+                    display: flex;
+                    justify-content: left;
+                    .icon {
+                        height: 26px;
+                        width: 26px;
+                        color: #1d1d1d;
+                    }
+                    p {
+                        margin-left: 10px;
+                    }
+                }
+                .disabled {
+                    cursor: not-allowed !important;
+                }
+            }
+        }
+    }
+    .expanded-enter {
+        background: rgba(0, 0, 0, 0);
+        .container {
+            height: 0;
+        }
+    }
+    .expanded-enter-active {
+        background: rgba(0, 0, 0, 0.15);
+        transition: background 250ms;
+        .container {
+            height: ${MENU_HEIGHT};
+            transition: height 250ms;
+        }
+    }
+    .expanded-exit {
+        background: rgba(0, 0, 0, 0.15);
+        cursor: auto;
+        .container {
+            height: ${MENU_HEIGHT};
+        }
+    }
+    .expanded-exit-active {
+        background: rgba(0, 0, 0, 0);
+        transition: background 250ms;
+        cursor: auto;
+        .container {
+            height: 0;
+            transition: height 250ms;
+        }
+    }
+`;
+
 function Sidebar() {
-    return (
-        <StyledSidebar>
-            {/* Logo */}
-            <Link href="/">
-                <a className="logo">
-                    <img src="/images/logo192.png" alt="" />
-                </a>
-            </Link>
-            <div className="tabs">
-                {/* Metrics */}
+    const [isMobile, setMobile] = useState();
+    const [isExpanded, setExpanded] = useState();
+
+    // Populating isMobile state
+    useEffect(() => {
+        function onResize() {
+            setMobile(window.innerWidth < 620);
+        }
+        onResize();
+        window.addEventListener("resize", onResize);
+        return () => {
+            window.removeEventListener("resize", onResize);
+        };
+    }, []);
+
+    // Closing menu when transitioning from desktop to mobile
+    useEffect(() => {
+        setExpanded(false);
+    }, [isMobile]);
+
+    if (isMobile) {
+        // Return mobile
+        return (
+            <StyledHamburger isExpanded={isExpanded}>
+                {/* Open and Close buttons */}
+                <Button
+                    onClick={() => setExpanded(!isExpanded)}
+                    className="hamburgerButton"
+                    size="small"
+                >
+                    {isExpanded ? (
+                        <FiX className="hamburger" />
+                    ) : (
+                        <FiBarChart2 className="hamburger" />
+                    )}
+                </Button>
+                {/* Actual menu to be rendered when expanded */}
+                <CSSTransition in={isExpanded} classNames="expanded" unmountOnExit timeout={250}>
+                    <div className="expanded">
+                        <ClickAwayListener onClickAway={() => setExpanded(false)}>
+                            <div className="container">
+                                {/* Metrics */}
+                                <Link href="/">
+                                    <div className="item">
+                                        <Button className="button">
+                                            <FiPieChart className="icon" />
+                                            <p>Metrics</p>
+                                        </Button>
+                                    </div>
+                                </Link>
+                                {/* Finances */}
+                                <div className="item">
+                                    <Button disableRipple className="button disabled">
+                                        <MdAttachMoney className="icon" />
+                                        <p>Finances</p>
+                                    </Button>
+                                </div>
+                                {/* Admin panel */}
+                                <Link href="/admin">
+                                    <div className="item">
+                                        <Button className="button">
+                                            <FiEdit2 className="icon" />
+                                            <p>Admin Panel</p>
+                                        </Button>
+                                    </div>
+                                </Link>
+                                {/* Log out */}
+                                <Link href="/login">
+                                    <div className="item">
+                                        <Button className="button">
+                                            <FiLogOut className="icon" />
+                                            <p>Log Out</p>
+                                        </Button>
+                                    </div>
+                                </Link>
+                            </div>
+                        </ClickAwayListener>
+                    </div>
+                </CSSTransition>
+            </StyledHamburger>
+        );
+    } else if (!isMobile && isMobile !== undefined) {
+        // Return desktop
+        return (
+            <StyledSidebar>
+                {/* Logo */}
                 <Link href="/">
-                    <Tooltip title="Metrics" arrow placement="right">
+                    <a className="logo">
+                        <img src="/images/logo192.png" alt="" />
+                    </a>
+                </Link>
+                <div className="tabs">
+                    {/* Metrics */}
+                    <Link href="/">
+                        <Tooltip title="Metrics" arrow placement="right">
+                            <Button className="button">
+                                <FiPieChart className="icon" />
+                            </Button>
+                        </Tooltip>
+                    </Link>
+                    {/* Finances */}
+                    <Tooltip title="Finances" arrow placement="right">
+                        <Button disableRipple className="button disabled">
+                            <MdAttachMoney className="icon" />
+                        </Button>
+                    </Tooltip>
+                    {/* Admin panel */}
+                    <Link href="/admin">
+                        <Tooltip title="Admin panel" arrow placement="right">
+                            <Button className="button">
+                                <FiEdit2 className="icon" />
+                            </Button>
+                        </Tooltip>
+                    </Link>
+                </div>
+                {/* Logout */}
+                <Link href="/login">
+                    <Tooltip title="Log out" arrow placement="right">
                         <Button className="button">
-                            <FiPieChart className="icon" />
+                            <FiLogOut className="icon" />
                         </Button>
                     </Tooltip>
                 </Link>
-                {/* Finances */}
-                <Tooltip title="Finances" arrow placement="right">
-                    <Button disableRipple className="button disabled">
-                        <MdAttachMoney className="icon" />
-                    </Button>
-                </Tooltip>
-                {/* Admin panel */}
-                <Link href="/admin">
-                    <Tooltip title="Admin panel" arrow placement="right">
-                        <Button className="button">
-                            <FiEdit2 className="icon" />
-                        </Button>
-                    </Tooltip>
-                </Link>
-            </div>
-            {/* Logout */}
-            <Link href="/login">
-                <Tooltip title="Log out" arrow placement="right">
-                    <Button className="button">
-                        <FiLogOut className="icon" />
-                    </Button>
-                </Tooltip>
-            </Link>
-        </StyledSidebar>
-    );
+            </StyledSidebar>
+        );
+    } else {
+        return null;
+    }
 }
 
 export default Sidebar;
