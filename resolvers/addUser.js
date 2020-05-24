e8a242d8acab601522b9441c365803d884c0808a
Seth Gaglione <seth.gaglione@live.com>
2020-05-24 14:47:55 +0000

    addUser checks for unique name and password

--- resolvers/addUser.js
+++ resolvers/addUser.js
@@ -1,9 +1,36 @@
 /**
- * Creates a new password with the company name and a unique password
+ * Creates a new password with the company name and a password
  */
 
 const addUser = async (_, { company, password }) => {
     const { keystone } = require("../index.js");
+
+    //Check to see if the company name was already used
+    const companyCheck = await keystone.executeQuery(`
+            query {
+                allUsers(where:{company:"${company}"}) {
+                    company
+                }
+            }
+        `);
+    //Checks to see if any users with the same company name was returned
+    if(companyCheck.data.allUsers.length > 0){
+        return null;
+    }
+
+    //Check to see if the password was already used
+    const passwordCheck = await keystone.executeQuery(`
+        query {
+            allUsers(where:{password:"${password}"}) {
+                company
+            }
+        }
+    `);
+
+    //Checks to see if any users with the same password was returned
+    if(passwordCheck.data.allUsers.length > 0){
+        return null;
+    }
 
     // Create page
     const result = await keystone.executeQuery(`
@@ -18,6 +45,7 @@ const addUser = async (_, { company, password }) => {
             }
         }
     `);
+
     return result.data.createUser;
 };
 
