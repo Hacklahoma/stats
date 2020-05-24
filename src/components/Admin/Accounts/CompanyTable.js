0a71e04be0e10e3eb1cca9586de9136649c4e7a1
s-fletcher <sfletchertx@gmail.com>
2020-05-24 13:08:47 -0500

    Implemented drop down

--- src/components/Admin/Accounts/CompanyTable.js
+++ src/components/Admin/Accounts/CompanyTable.js
@@ -1,13 +1,36 @@
 import styled from "styled-components";
-import { Table, TableHead, TableRow, TableCell, TableBody } from "@material-ui/core";
+import {
+    Table,
+    TableHead,
+    TableRow,
+    TableCell,
+    TableBody,
+    Menu,
+    MenuItem,
+    Button,
+} from "@material-ui/core";
+import { FiMoreHorizontal } from "react-icons/fi";
 
 const StyledTable = styled.div`
     .MuiTableCell-root {
         font-family: inherit !important;
     }
+    .MuiButton-root {
+        min-width: 42px;
+    }
 `;
 
 function CompanyTable({ rows }) {
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
     return (
         <StyledTable>
             <Table size="small" aria-label="a dense table">
@@ -22,10 +45,53 @@ function CompanyTable({ rows }) {
                 <TableBody>
                     {rows.map((row) => (
                         <TableRow key={row.name}>
-                            <TableCell style={{ paddingLeft: "10px", fontWeight: "bold" }}>{row.name}</TableCell>
-                            <TableCell>{row.views}</TableCell>
+                            <TableCell style={{ paddingLeft: "10px", fontWeight: "bold" }}>
+                                {row.name}
+                            </TableCell>
                             <TableCell>{row.status}</TableCell>
-                            <TableCell align="right">•••</TableCell>
+                            <TableCell>{row.views}</TableCell>
+                            <TableCell align="right">
+                                <Button size="small" onClick={handleClick}>
+                                    <FiMoreHorizontal size="22px" />
+                                </Button>
+                                <Menu
+                                    anchorEl={anchorEl}
+                                    keepMounted
+                                    elevation={0}
+                                    style={{
+                                        margin: "43px 0 0 -30px",
+                                    }}
+                                    open={Boolean(anchorEl)}
+                                    onClose={handleClose}
+                                >
+                                    <MenuItem
+                                        style={{ color: "#1d1d1d", fontSize: ".85em" }}
+                                        onClick={handleClose}
+                                    >
+                                        Copy Password
+                                    </MenuItem>
+                                    <MenuItem
+                                        style={{ color: "#1d1d1d", fontSize: ".85em" }}
+                                        onClick={handleClose}
+                                    >
+                                        Rename
+                                    </MenuItem>
+                                    <div
+                                        style={{
+                                            width: "100%",
+                                            margin: "4px 0",
+                                            background: "rgba(0,0,0,0.2)",
+                                            minHeight: "1px",
+                                        }}
+                                    />
+                                    <MenuItem
+                                        style={{ color: "#e81c0e", fontSize: ".85em" }}
+                                        onClick={handleClose}
+                                    >
+                                        Disable
+                                    </MenuItem>
+                                </Menu>
+                            </TableCell>
                         </TableRow>
                     ))}
                 </TableBody>
