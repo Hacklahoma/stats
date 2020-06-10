0b26c6981dba51be2e2dc774a079852dcad57e13
s-fletcher <sfletchertx@gmail.com>
2020-06-10 18:28:59 -0500

    Removed add view

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { KnexAdapter: Adapter } = require("@keystonejs/adapter-knex");
 const { PasswordAuthStrategy } = require("@keystonejs/auth-password");
 const { NextApp } = require("@keystonejs/app-next");
 const { User, Event, Year, Hacker, Admin, Metric } = require("./models");
-const { addUser, changeUser, login, uploadYear, addEvent, addView } = require("./resolvers");
+const { addUser, changeUser, login, uploadYear, addEvent } = require("./resolvers");
 const keepAwake = require("./src/lib/keepAwake");
 
 // Get environmental variables
@@ -65,10 +65,6 @@ keystone.extendGraphQLSchema({
             schema: "addEvent(id: ID!, type: String!, description: String): Event",
             resolver: addEvent,
         },
-        {
-            schema: "addView(id: ID!): Event",
-            resolver: addView,
-        }
     ],
 });
 
