5e0677f7e1bdae606ece4045fe974ef02dea1060
s-fletcher <sfletchertx@gmail.com>
2020-06-10 14:21:24 -0500

    Simplified add view resolver

--- resolvers/addView.js
+++ resolvers/addView.js
@@ -2,46 +2,34 @@
  * Takes in an id and adds the view event to the company
  */
 
-
 const addView = async (_, { id }) => {
     const { keystone } = require("../index.js");
-
+ 
     //Create the view from the user's id
     const event = await keystone.executeQuery(`
-        mutation {
-            createEvent(
-                data:{
-                    parent: {connect: {id: ${id}}}, 
-                    type: VIEW,
-                    description: "Company View",
-                    timestamp: "${(new Date()).toISOString()}",
-                }
-            ){
-                id
-                parent { id company }
-                type 
-                description
-                timestamp
-            }
-        }
-    `);
-
-    //Update the user to include the new event
-    const updatedUser = await keystone.executeQuery(`
         mutation {
             updateUser(
                 id: ${id},
                 data: {
-                    activity: {connect: {id: ${event.data.createEvent.id}}},
+                    activity: {
+                        create: {
+                            parent: {connect: {id: ${id}}},
+                            type: VIEW,
+                            description: "Company View",
+                            timestamp: "${new Date().toISOString()}"
+                        }
+                    }
                 }
-            ){
+            ) {
                 id
-                activity { description }
+                activity {
+                    id
+                }
             }
         }
     `);
 
-    return event.data.createEvent;
-}
+    return event.data.updateUser;
+};
 
 module.exports = addView;
